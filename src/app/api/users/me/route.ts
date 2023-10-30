73f189b75afd907d3f743b19b86236829a98dc9f
“Rohit <webxgrafix@gmail.com>
2023-10-30 18:18:08 +0530

    added middleware and me route

---
+++ src/app/api/users/me/route.ts
@@ -0,0 +1,21 @@
+import { getDataFromToken } from "@/helpers/getDataFromToken";
+
+import { NextResponse,NextRequest } from "next/server";
+import User from "@/lib/model/UserModel";
+import connectDB from "@/lib/dbConnection/dbconfig";
+
+connectDB();
+
+export async function GET(request: NextRequest) {
+    try {
+       const userId = await getDataFromToken(request);
+       const user = await User.findOne({_id: userId}).select("-password")
+       
+       return NextResponse.json({
+        message: "User Found",
+        data: user
+       })
+    } catch (error: any) {
+        return NextResponse.json({error: error.message}, {status: 400})
+    }
+}
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,11 +1,13 @@
 "use client";
-import React from "react";
+import React, {useState} from "react";
 import axios from "axios";
 import toast from "react-hot-toast";
 import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 const Profilepage = () => {
   const router = useRouter();
+  const [data, setData] = useState("nothing")
   const logout = async () => {
     try {
       await axios.get("/api/users/logout");
@@ -17,11 +19,19 @@ const Profilepage = () => {
     }
   };
 
+  const getUserDetails = async () => {
+    const res = await axios.get('/api/users/me')
+    console.log(res.data)
+    setData(res.data.data._id)
+  }
+
   return (
     <div>
       <h1>profile page</h1>
+      <h2>{data === "nothing" ? "Nothing" : <Link href={`/profile/${data}`}>{data}</Link> }</h2>
       <hr />
       <button onClick={logout}>logout</button>
+      <button onClick={getUserDetails}>Getuser Details</button>
     </div>
   );
 };
